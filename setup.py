b105741cb2f437d358e7c53a8b88f48fef5bf987
Joona Pääkkönen <joona@giosg.com>
2016-06-20 12:36:35 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pytelegraf',
-    version='0.1.0',
+    version='0.1.1',
     description='Telegraf client',
     author='paksu',
     url='https://github.com/paksu/pytelegraf',
--- telegraf/__init__.py
+++ telegraf/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 from .client import TelegrafClient
 
-VERSION = (0, 1, 0)
+VERSION = (0, 1, 1)
 __version__ = '.'.join(map(str, VERSION))
 __all__ = ['TelegrafClient']
