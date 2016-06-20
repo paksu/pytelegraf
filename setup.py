e80ef3cbdda09e2d257af185b2a05d234ae6b1be
Joona Pääkkönen <joona@giosg.com>
2016-06-20 10:58:51 +0300

    Updated classifiers

--- setup.py
+++ setup.py
@@ -11,6 +11,16 @@ setup(
     include_package_data=True,
     test_suite='telegraf.tests',
     classifiers=[
-        'foo'
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
